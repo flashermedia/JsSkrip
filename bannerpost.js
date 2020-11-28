80a7ab89ba7d2041a808ff40f87e07fd840275c4
flashermedia <41857634+flashermedia@users.noreply.github.com>
2020-11-29 04:08:42 +0700

    Update bannerpost.js

--- bannerpost.js
+++ bannerpost.js
@@ -1,12 +1,10 @@
 <script type="text/javascript">
   var popupbanner=new Array()  
-popupbanner[0]=&#39;https://ir3.xyz/5fbb28cf61639|https://app.indoleads.com/uploads/5aeaecf271eac.jpeg|DX.com - World&#39;
-popupbanner[1]=&#39;https://ir3.xyz/5fbb297bd1b62|https://app.indoleads.com/uploads/5ee1be25825e9.jpeg|Chic Me - World&#39;
-popupbanner[2]=&#39;https://ir3.xyz/5fbb2e6f8016f|https://app.indoleads.com/uploads/5b39bb5fd404a.png|Tomtop.com - World&#39;
-popupbanner[3]=&#39;https://ir3.xyz/5fbb2fc60f137|https://app.indoleads.com/uploads/5fbb442c7a919.jpeg|YOINS - World&#39;
-popupbanner[4]=&#39;https://ir3.xyz/5fba76cf369d9|https://app.indoleads.com/uploads/5bf632c108e90.jpeg|Banggood.com - World&#39;
+popupbanner[0]=&#39;https://ir3.xyz/5fba7682990ef|https://app.indoleads.com/uploads/5f4776f568b23.jpeg|DX.com - World&#39;
+popupbanner[1]=&#39;https://ir3.xyz/5fba7682990ef|https://app.indoleads.com/uploads/5f4776f56ba09.jpeg|Chic Me - World&#39;
+popupbanner[2]=&#39;https://ir3.xyz/5fba7682990ef|https://app.indoleads.com/uploads/5f58917ab3ea4.png|Tomtop.com - World&#39;
+popupbanner[3]=&#39;https://ir3.xyz/5fba7682990ef|https://app.indoleads.com/uploads/5f7436ba040c8.jpeg|YOINS - World&#39;
  
-  
  var whichpopupbanner=Math.floor(Math.random()*(popupbanner.length));  
   var StrInduk = popupbanner[whichpopupbanner];
  var res = StrInduk.split(&quot;|&quot;);
